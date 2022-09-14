ec867e676e74ee632b83986ddda038e12daafbaa
sajeev.srinivasan <sajeev.srinivasan@thoughtworks.com>
2022-09-14 10:06:13 +0530

    Initial Commit

---
+++ MoviesCentre/MoviesCentreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCentreApp.swift
+//  MoviesCentre
+//
+//  Created by Sajeev S on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesCentreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
