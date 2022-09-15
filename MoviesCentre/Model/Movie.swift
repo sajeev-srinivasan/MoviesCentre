74647742e207b379c16af338dd644397caac19e2
sajeev.srinivasan <sajeev.srinivasan@thoughtworks.com>
2022-09-15 11:39:36 +0530

    Add. Add movie card view file

--- MoviesCentre/Model/Movie.swift
+++ MoviesCentre/Model/Movie.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Movie:Equatable {
+struct Movie:Equatable, Hashable{
     var title: String
     var description: String
     var rating: Float
---
+++ MoviesCentre/Views/MovieCardView.swift
@@ -0,0 +1,50 @@
+//
+//  MovieCardView.swift
+//  MoviesCentre
+//
+//  Created by Sajeev S on 14/09/22.
+//
+
+import SwiftUI
+
+struct MovieCardView: View {
+    
+    var movieURL: String = "https://m.media-amazon.com/images/M/MV5BMTg0NTM3MTI1MF5BMl5BanBnXkFtZTgwMTAzNTAzNzE@._V1_.jpg"
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            
+            AsyncImage(url: URL(string: movieURL)) { phase in
+                if let image = phase.image {
+                    image.resizable().cornerRadius(20)
+                }
+                
+                else if phase.error != nil {
+                    Image(systemName: "nosign.app.fill")
+                        .resizable()
+                        .foregroundColor(Color.red)
+                        .frame(width: 25, height: 25, alignment: .center)
+                    
+                    Text("Preview not available")
+                        .padding(/*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                }
+                else{
+                    ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                }
+            }
+        }
+        .background(.black)
+        .aspectRatio(2/3, contentMode: .fit)
+    }
+}
+
+
+struct MovieCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCardView()
+    }
+}
